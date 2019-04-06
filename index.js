76751e92c35998258e60d811025a336890d333dd
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-06 22:13:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function prependKitten(name){
   return [name,...kittens];
 }
 function removeLastKitten(){
-  return kittens.slice(0, kittens.lenght-2);
+  return kittens.slice(0, kittens.lenght-1);
 }
 function removeFirstKitten(kittens){
   return kittens.slice(1);
